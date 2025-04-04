ae543a1b033d53cdeed7a6554c80855f85f472db
czhewei <czhewei.work@gmail.com>
2025-04-04 14:19:00 +0800

    re-run npm run build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   output: "export",
-  basePath: isProd ? "/personal-website" : "", 
-  assetPrefix: isProd ? "/personal-website" : ""
+  basePath: isProd ? "/personal-website" : "",
+  assetPrefix: isProd ? "/personal-website" : "",
 };
