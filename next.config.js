ced012ac2cb6122d0409f109ab2c8e5c6284165e
czhewei <czhewei.work@gmail.com>
2025-04-05 15:21:51 +0800

    fix assets issues

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const basePath = isProd ? "/personal-website" : "";
 
 module.exports = {
   output: "export",
